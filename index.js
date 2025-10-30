aefffb2c58b3c9e6558c18fcb228e831972bf391
Roman BOBKO <147614380+RomBobko@users.noreply.github.com>
2025-10-30 15:35:45 +0200

    Base html/css/js

--- index.js
+++ index.js
@@ -24,24 +24,86 @@ const refs = {
 
 refs.keypad.addEventListener('click', keypadClickHandler);
 
+let current = '';
+let previous = '';
+let operator = null;
+let justEvalueted = false;
+
 function keypadClickHandler(event) {
     const btn = event.target;
 
-    if()
+    if (!btn.closest('button')) return;
 
     if (btn.dataset.number) {
-        console.log('number');
+        numberBtnHandler(btn.dataset.number);
     }
     if (btn.dataset.op) {
-        console.log('operation');
+        operationBtnHandler(btn.dataset.op);
     }
     if (btn.dataset.clear) {
-        console.log('Clear');
+        clearBtnHandler();
     }
     if (btn.dataset.equals) {
-        console.log('equal');
+        equalsBtnHandler();
     }
 
-    console.log('ловить');
-    
+    render();
+}
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => (b === 0 ? 'Error' : a / b);
+
+function compute(a, b, operator) {
+    a = Number(a);
+    b = Number(b);
+
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return sub(a, b);
+        case '*':
+            return mul(a, b);
+        case '/':
+            return div(a, b);
+
+        default:
+            return 'Error';
+    }
+}
+
+function numberBtnHandler(digit) {
+    if (justEvalueted) {
+        current = '';
+        justEvalueted = false;
+    }
+
+    current += digit;
+}
+
+function operationBtnHandler(op) {
+    previous = current;
+    current = '';
+    operator = op;
+}
+
+function clearBtnHandler(params) {
+    current = '';
+    previous = '';
+    operator = null;
+}
+
+function equalsBtnHandler() {
+    const result = compute(previous, current, operator);
+    current = result.toString();
+    previous = '';
+    operator = null;
+    justEvalueted = true;
+}
+
+function render() {
+    refs.totalValue.textContent =
+        `${previous} ${operator || ''} ${current} `.trim() || '0';
 }
